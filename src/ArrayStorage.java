126624b8b1aa496fb1ee64756bca55b1fe9f35fe
tomafoks <tomafoks@mail.ru>
2025-09-16 10:21:08 +0500

    lesson 1 done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,26 +7,49 @@ public class ArrayStorage {
     private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < size; i++) {
-            
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                size++;
+                break;
+            }
         }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                storage[size] = null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resume = new Resume[size];
+        System.arraycopy(storage, 0, resume, 0, size);
+        return resume;
     }
 
     int size() {
